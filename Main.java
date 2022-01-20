bc3f96f323fdcf557ea56be685313b1a1238ef89
WesleyTavDev <91393560+WesleyTavaresGameDev@users.noreply.github.com>
2022-01-20 20:46:03 +0000

    Extract Function => In the first loop that print my array in horizontal direction

--- Main.java
+++ Main.java
@@ -1,18 +1,12 @@
-class Main {
+class Main 
+{
   public static void main(String[] args) 
   {
+    Main mainClass = new Main();
+
     int[] numbers = {1, 2, 3, 4, 5, 6};
 
-    for(int i = 0; i < numbers.length; i++)
-    {
-      if(i < numbers.length - 1)
-        System.out.print(numbers[i] + " - ");
-      else
-      {
-        System.out.print(numbers[i]);
-        System.out.println();
-      }
-    }
+    mainClass.PrintHorizontalNumbers(numbers);
 
      System.out.println();
     
@@ -27,5 +21,20 @@ class Main {
     System.out.println("      *    ");
     System.out.println("      *    ");
     System.out.println("      *    ");
+  }
+
+  void PrintHorizontalNumbers(int[] numbers)
+  {
+    for(int i = 0; i < numbers.length; i++)
+      {
+        if(i < numbers.length - 1)
+          System.out.print(numbers[i] + " - ");
+        else
+        {
+          System.out.print(numbers[i]);
+          System.out.println();
+        }
+        
+    }
   }
 }
