02407e7fa2b8b9bd8383ae2e9678371ad48b71bf
WesleyTavDev <91393560+WesleyTavaresGameDev@users.noreply.github.com>
2022-01-20 20:59:34 +0000

    Extract Function => In print vertical numbers and my tree

--- Main.java
+++ Main.java
@@ -3,16 +3,31 @@ class Main
   public static void main(String[] args) 
   {
     Main mainClass = new Main();
-
     int[] numbers = {1, 2, 3, 4, 5, 6};
 
     mainClass.PrintHorizontalNumbers(numbers);
 
-     System.out.println();
+    System.out.println();
     
-    for(int j = 0; j < numbers.length; j++)
-        System.out.println(numbers[j]);
+    mainClass.PrintVerticalNumbers(numbers);
+    mainClass.PrintTree();
+  }
 
+  void PrintHorizontalNumbers(int[] numbers)
+  {
+    for(int i = 0; i < numbers.length; i++)   
+      System.out.print(i < numbers.length - 1 ? numbers[i] + " - " : numbers[i]); 
+  }
+
+  void PrintVerticalNumbers(int[] numbers)
+  {
+    System.out.println();
+    for(int j = 0; j < numbers.length; j++)
+      System.out.println(numbers[j]);
+  }
+
+  void PrintTree()
+  {
     System.out.println("      *    ");
     System.out.println("     ***   ");
     System.out.println("    *****  ");
@@ -21,11 +36,5 @@ class Main
     System.out.println("      *    ");
     System.out.println("      *    ");
     System.out.println("      *    ");
-  }
-
-  void PrintHorizontalNumbers(int[] numbers)
-  {
-    for(int i = 0; i < numbers.length; i++)   
-      System.out.print(i < numbers.length - 1 ? numbers[i] + " - " : numbers[i]); 
   }
 }
